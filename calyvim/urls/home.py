557c0cecfe4d71c12ef2cd4ff3166f7bd76c0295
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2024-12-27 14:16:30 +0530

    feat(home): remove home URL patterns for cleanup

--- calyvim/urls/home.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from calyvim.views.home import index, terms
-
-urlpatterns = [
-    path("terms/", terms, name="home-terms"),
-    path("", index, name="home-index"),
-]
