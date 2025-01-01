a28e5d329a3a0054fb4a08e9a09cf3e49f03be22
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-01-01 12:15:46 +0530

    feat(tabs): convert tab labels to links for improved navigation and accessibility

--- calyvim/static/components/base/board-settings-layout.vue
+++ calyvim/static/components/base/board-settings-layout.vue
@@ -1,81 +1,142 @@
 <script setup>
-import BoardLayout from '@/components/base/board-layout.vue';
-import { BarsOutlined, HddOutlined, NodeExpandOutlined, ProjectOutlined, TagsOutlined, TeamOutlined } from '@ant-design/icons-vue';
-import { Layout, LayoutContent, LayoutSider, Menu, MenuItem } from 'ant-design-vue';
-import { ref } from 'vue';
+import BoardLayout from '@/components/base/board-layout.vue'
+import {
+  BarsOutlined,
+  HddOutlined,
+  NodeExpandOutlined,
+  ProjectOutlined,
+  TagsOutlined,
+  TeamOutlined,
+} from '@ant-design/icons-vue'
+import {
+  Layout,
+  LayoutContent,
+  LayoutSider,
+  Menu,
+  MenuItem,
+} from 'ant-design-vue'
+import { ref } from 'vue'
 
 const props = defineProps(['workspace', 'board', 'page'])
 const collapsed = ref(false)
 const selectedKeys = ref([props.page])
 
 const changePage = (event) => {
-    if (event.key === 'general') {
-        window.location.href = `/app/b/${props.board.id}/settings`
-    } else {
-        window.location.href = `/app/b/${props.board.id}/settings/${event.key}/`
-    }
+  if (event.key === 'general') {
+    window.location.href = `/app/b/${props.board.id}/settings`
+  } else {
+    window.location.href = `/app/b/${props.board.id}/settings/${event.key}/`
+  }
 }
 </script>
 
 <template>
-    <BoardLayout :workspace="props.workspace" :board="props.board" page="settings">
-        <div class="container mx-auto">
-            <Layout v-model:collapsed="collapsed" :trigger="null" collapsible>
-                <LayoutSider>
-                    <Menu v-model:selectedKeys="selectedKeys" mode="inline" @select="changePage">
-                        <MenuItem key="general">
-                            <BarsOutlined />
-                            <span>General</span>
-                        </MenuItem>
+  <BoardLayout
+    :workspace="props.workspace"
+    :board="props.board"
+    page="settings"
+  >
+    <div class="container mx-auto">
+      <Layout v-model:collapsed="collapsed" :trigger="null" collapsible>
+        <LayoutSider>
+          <Menu v-model:selectedKeys="selectedKeys" mode="inline">
+            <MenuItem key="general">
+              <a
+                :href="`/app/b/${props.board.id}/settings`"
+                class="no-underline text-inherit hover:no-underline hover:text-inherit"
+                :class="{
+                  'text-primary hover:text-primary':
+                    selectedKeys[0] === 'general',
+                }"
+              >
+                <BarsOutlined />
+                <span>General</span>
+              </a>
+            </MenuItem>
 
-                        <MenuItem key="collaborators">
-                            <TeamOutlined />
-                            <span>Collaborators</span>
-                        </MenuItem>
+            <MenuItem key="collaborators">
+              <a
+                :href="`/app/b/${props.board.id}/settings/collaborators`"
+                class="no-underline text-inherit hover:no-underline hover:text-inherit"
+                :class="{
+                  'text-primary hover:text-primary':
+                    selectedKeys[0] === 'collaborators',
+                }"
+              >
+                <TeamOutlined />
+                <span>Collaborators</span>
+              </a>
+            </MenuItem>
 
-                        <MenuItem key="states">
-                            <HddOutlined />
-                            <span>States</span>
-                        </MenuItem>
+            <MenuItem key="states">
+              <a
+                :href="`/app/b/${props.board.id}/settings/states`"
+                class="no-underline text-inherit hover:no-underline hover:text-inherit"
+                :class="{
+                  'text-primary hover:text-primary':
+                    selectedKeys[0] === 'states',
+                }"
+              >
+                <HddOutlined />
+                <span>States</span>
+              </a>
+            </MenuItem>
 
-                        <MenuItem key="priorities">
-                            <NodeExpandOutlined />
-                            <span>Priorities</span>
-                        </MenuItem>
+            <MenuItem key="priorities">
+              <a
+                :href="`/app/b/${props.board.id}/settings/priorities`"
+                class="no-underline text-inherit hover:no-underline hover:text-inherit"
+                :class="{
+                  'text-primary hover:text-primary':
+                    selectedKeys[0] === 'priorities',
+                }"
+              >
+                <NodeExpandOutlined />
+                <span>Priorities</span>
+              </a>
+            </MenuItem>
 
-                        <MenuItem key="labels">
-                            <TagsOutlined />
-                            <span>Labels</span>
-                        </MenuItem>
-                    </Menu>
-                </LayoutSider>
+            <MenuItem key="labels">
+              <a
+                :href="`/app/b/${props.board.id}/settings/labels`"
+                class="no-underline text-inherit hover:no-underline hover:text-inherit"
+                :class="{
+                  'text-primary hover:text-primary':
+                    selectedKeys[0] === 'labels',
+                }"
+              >
+                <TagsOutlined />
+                <span>Labels</span>
+              </a>
+            </MenuItem>
+          </Menu>
+        </LayoutSider>
 
-                <Layout>
-                    <LayoutContent>
-                        <slot></slot>
-                    </LayoutContent>
-                </Layout>
-            </Layout>
-        </div>
-
-    </BoardLayout>
+        <Layout>
+          <LayoutContent>
+            <slot></slot>
+          </LayoutContent>
+        </Layout>
+      </Layout>
+    </div>
+  </BoardLayout>
 </template>
 
 <style scoped>
 .ant-layout-sider {
-    min-height: 100vh;
-    background: #fff;
-    border-right: 1px solid #a9a9a9;
+  min-height: 100vh;
+  background: #fff;
+  border-right: 1px solid #a9a9a9;
 }
 
 .ant-layout-content {
-    padding: 18px;
-    background: #fff;
+  padding: 18px;
+  background: #fff;
 }
 
 .ant-layout-header {
-    background: #fff;
-    padding: 0;
-    border-bottom: 1px solid gray;
+  background: #fff;
+  padding: 0;
+  border-bottom: 1px solid gray;
 }
 </style>
--- calyvim/static/components/base/workspace-settings-layout.vue
+++ calyvim/static/components/base/workspace-settings-layout.vue
@@ -1,81 +1,117 @@
 <script setup>
-import WorkspaceLayout from '@/components/base/workspace-layout.vue';
-import { ApiOutlined, CreditCardOutlined, ExportOutlined, ProfileOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons-vue';
-import { Avatar, TabPane, Tabs } from 'ant-design-vue';
-import { ref, computed } from 'vue';
-import { generateAvatar } from '@/utils/helpers';
+import WorkspaceLayout from '@/components/base/workspace-layout.vue'
+import {
+  ApiOutlined,
+  CreditCardOutlined,
+  ExportOutlined,
+  ProfileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons-vue'
+import { Avatar, TabPane, Tabs } from 'ant-design-vue'
+import { ref, computed } from 'vue'
+import { generateAvatar } from '@/utils/helpers'
 
 const props = defineProps(['workspace', 'currentUser', 'page'])
 const activeKey = ref(props.page)
 
 const switchTab = (key) => {
-    var redirectUrl = `/app/${props.workspace.slug}/settings/${key}/`
+  var redirectUrl = `/app/${props.workspace.slug}/settings/${key}/`
 
-    if (key === 'general') {
-        redirectUrl = `/app/${props.workspace.slug}/settings/`
-    }
+  if (key === 'general') {
+    redirectUrl = `/app/${props.workspace.slug}/settings/`
+  }
 
-    window.location.href = redirectUrl
+  window.location.href = redirectUrl
 }
 
 const getWorkspaceLogo = computed(() => {
-    if (props.workspace?.logo) {
-        return props.workspace.logo
-    }
+  if (props.workspace?.logo) {
+    return props.workspace.logo
+  }
 
-    return generateAvatar(props.workspace.name, 20)
+  return generateAvatar(props.workspace.name, 20)
 })
-
 </script>
 
 <template>
-    <WorkspaceLayout page="settings" :workspace="props.workspace" :currentUser="props.currentUser">
-        <div class="container mx-auto p-2">
-            <div class="text-xl font-semibold mb-2">{{ props.workspace.name }}'s settings</div>
-            <Tabs v-model:activeKey="activeKey" @change="switchTab">
-                <TabPane key="general">
-                    <template #tab>
-                        <ProfileOutlined />
-                        <span>General</span>
-                    </template>
-                </TabPane>
+  <WorkspaceLayout
+    page="settings"
+    :workspace="props.workspace"
+    :currentUser="props.currentUser"
+  >
+    <div class="container mx-auto p-2">
+      <div class="text-xl font-semibold mb-2">
+        {{ props.workspace.name }}'s settings
+      </div>
+      <Tabs v-model:activeKey="activeKey">
+        <TabPane key="general">
+          <template #tab>
+            <a
+              :href="`/app/${props.workspace.slug}/settings/`"
+              class="no-underline text-inherit hover:no-underline hover:text-inherit"
+              :class="{
+                'text-primary hover:text-primary': activeKey === 'general',
+              }"
+            >
+              <ProfileOutlined />
+              <span>General</span>
+            </a>
+          </template>
+        </TabPane>
 
-                <TabPane key="members">
-                    <template #tab>
-                        <UserOutlined />
-                        <span>Members</span>
-                    </template>
-                </TabPane>
+        <TabPane key="members">
+          <template #tab>
+            <a
+              :href="`/app/${props.workspace.slug}/settings/members/`"
+              class="no-underline text-inherit hover:no-underline hover:text-inherit"
+              :class="{
+                'text-primary hover:text-primary': activeKey === 'members',
+              }"
+            >
+              <UserOutlined />
+              <span>Members</span>
+            </a>
+          </template>
+        </TabPane>
 
-                <TabPane key="teams">
-                    <template #tab>
-                        <TeamOutlined />
-                        <span>Teams</span>
-                    </template>
-                </TabPane>
+        <TabPane key="teams">
+          <template #tab>
+            <a
+              :href="`/app/${props.workspace.slug}/settings/teams/`"
+              class="no-underline text-inherit hover:no-underline hover:text-inherit"
+              :class="{
+                'text-primary hover:text-primary': activeKey === 'teams',
+              }"
+            >
+              <TeamOutlined />
+              <span>Teams</span>
+            </a>
+          </template>
+        </TabPane>
 
-                <TabPane key="integrations">
-                    <template #tab>
-                        <ApiOutlined />
-                        <span>Integrations</span>
-                    </template>
-                </TabPane>
+        <TabPane key="integrations" disabled>
+          <template #tab>
+            <ApiOutlined />
+            <span>Integrations</span>
+          </template>
+        </TabPane>
 
-                <TabPane key="billing">
-                    <template #tab>
-                        <CreditCardOutlined />
-                        <span>Billing</span>
-                    </template>
-                </TabPane>
+        <TabPane key="billing" disabled>
+          <template #tab>
+            <CreditCardOutlined />
+            <span>Billing</span>
+          </template>
+        </TabPane>
 
-                <TabPane key="exports">
-                    <template #tab>
-                        <ExportOutlined />
-                        <span>Exports</span>
-                    </template>
-                </TabPane>
-            </Tabs>
-            <slot></slot>
-        </div>
-    </WorkspaceLayout>
+        <TabPane key="exports" disabled>
+          <template #tab>
+            <ExportOutlined />
+            <span>Exports</span>
+          </template>
+        </TabPane>
+      </Tabs>
+      <slot></slot>
+    </div>
+  </WorkspaceLayout>
 </template>
