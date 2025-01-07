7666ade1b71f67c061475302c430af5a517dfff9
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-01-08 02:53:36 +0530

    feat(documents): add Block and Document models with permissions and relationships

---
+++ calyvim/models/block.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from calyvim.models.base import UUIDTimestampModel
+
+
+class Block(UUIDTimestampModel):
+    class BlockType(models.TextChoices):
+        HEADING_1 = ("heading_1", "Heading 1")
+        HEADING_2 = ("heading_2", "Heading 2")
+        HEADING_3 = ("heading_3", "Heading 3")
+        PARAGRAPH = ("paragraph", "Paragraph")
+        QUOTE = ("quote", "Quote")
+        TOGGLE = ("toggle", "Toggle")
+        BULLETED_LIST = ("bullet_list", "Bullet List")
+        NUMBERED_LIST = ("numbered_list", "Numbered List")
+        CODE = ("code", "Code")
+        LINK = ("link", "Link")
+        PAGE = ("page", "Page")
+
+    document = models.ForeignKey(
+        "Document", on_delete=models.CASCADE, related_name="blocks"
+    )
+    page = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="page_blocks",
+    )
+    block_type = models.CharField(max_length=24)
+    content = ArrayField(base_field=models.UUIDField(), default=list)
+    properties = models.JSONField()
+    created_by = models.ForeignKey(
+        "User", on_delete=models.SET_NULL, null=True, related_name="created_blocks"
+    )
+
+    class Meta:
+        db_table = "blocks"
+
+    def __str__(self):
+        return str(self.id)
