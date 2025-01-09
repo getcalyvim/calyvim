9463e50a4db8b017881a43fb0ae2813919304921
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-01-09 11:17:48 +0530

    feat(task-comments): enhance comment form layout and improve code readability

--- calyvim/static/components/boards/kanban/detail/task-comment-add-form.vue
+++ calyvim/static/components/boards/kanban/detail/task-comment-add-form.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { SaveOutlined } from '@ant-design/icons-vue'
-import { Button, Form, FormItem, message, Textarea, Input } from 'ant-design-vue'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+} from 'ant-design-vue'
 import { h, ref } from 'vue'
-import { handleResponseError, notify } from '@/utils/helpers';
-import { taskCommentsCreateAPI } from '@/utils/api';
+import { handleResponseError, notify } from '@/utils/helpers'
+import { taskCommentsCreateAPI } from '@/utils/api'
+import { MessageSquarePlus } from 'lucide-vue-next'
 
 const props = defineProps(['boardId', 'taskId'])
 const emit = defineEmits(['added'])
@@ -14,34 +19,54 @@ const form = ref({
 const formRef = ref()
 
 const onFinish = async (values) => {
-    try {
-        const { data } = await taskCommentsCreateAPI(props.boardId, props.taskId, values)
-        formRef.value.resetFields()
+  try {
+    const { data } = await taskCommentsCreateAPI(
+      props.boardId,
+      props.taskId,
+      values
+    )
+    formRef.value.resetFields()
 
-        emit('added', data.comment)
-        notify('ADDED', data.detail)
-    } catch (error) {
-        handleResponseError(error)
-    }
+    emit('added', data.comment)
+    notify('ADDED', data.detail)
+  } catch (error) {
+    handleResponseError(error)
+  }
 }
 </script>
 
 <template>
-  <Form
-    layout="horizontal"
-    :model="form"
-    name="task-comment-add"
-    @finish="onFinish"
-    hide-required-mark
-    ref="formRef"
-  >
-    <div class="flex justify-between gap-4">
-      <FormItem name="content" :rules="[{ message: 'Comment is requuired', required: true }]" class="w-full">
-        <Input v-model:value="form.content" placeholder="Add a comment.." />
-      </FormItem>
-      <FormItem>
-        <Button type="primary" html-type="submit" :icon="h(SaveOutlined)"> Comment</Button>
-      </FormItem>
+  <div class="grid grid-cols-12 gap-4">
+    <div class="col-span-9">
+      <Form
+        layout="horizontal"
+        :model="form"
+        name="task-comment-add"
+        @finish="onFinish"
+        hide-required-mark
+        ref="formRef"
+      >
+        <div class="flex justify-between gap-4">
+          <FormItem
+            name="content"
+            :rules="[{ message: 'Comment is requuired', required: true }]"
+            class="w-full p-0"
+          >
+            <Input v-model:value="form.content" placeholder="Add a comment.." />
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              html-type="submit"
+              :icon="
+                h(MessageSquarePlus, { class: 'w-4 h-4 align-middle mr-2' })
+              "
+            >
+              Comment</Button
+            >
+          </FormItem>
+        </div>
+      </Form>
     </div>
-  </Form>
+  </div>
 </template>
