773c47215451731b636d84625e2d9c0647483380
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-01-27 00:06:46 +0530

    feat(labels): enhance label filters with toggle visibility and limit displayed labels

--- calyvim/static/components/boards/kanban/filters/label-filters.vue
+++ calyvim/static/components/boards/kanban/filters/label-filters.vue
@@ -2,22 +2,36 @@
 import { Checkbox, CheckboxGroup, Tag } from 'ant-design-vue';
 import { useBoardStore } from '@/stores/board';
 import { TagTwoTone } from '@ant-design/icons-vue';
+import { computed, ref } from 'vue';
 
 const emit = defineEmits(['reload'])
 const store = useBoardStore()
+
+const showAllLabels = ref(false)
+
+const visibleLabels = computed(() => {
+    return showAllLabels.value ? store.labels : store.labels.slice(0, 5)
+})
+
+const toggleLabelVisibility = () => {
+    showAllLabels.value = !showAllLabels.value
+}
 </script>
 
 <template>
     <CheckboxGroup v-model:value="store.labelFilters" class="" @change="emit('reload')">
         <div class="flex flex-col gap-1">
-            <div class="flex items-center gap-2" v-for="label in store.labels" :key="label.id">
+            <div v-for="label in visibleLabels" :key="label.id" class="flex items-center gap-1">
                 <Checkbox :value="label.id"></Checkbox>
-                <div>
-                    <TagTwoTone :two-tone-color="label.color" />
-                    <span class="ml-2">
-                        {{ label.name }}
-                    </span>
-                </div>
+                <Tag :bordered="false" >
+                    <TagTwoTone :twoToneColor="label.color" />
+                    {{ label.name }}
+                </Tag>
+            </div>
+            <div v-if="store.labels.length > 5" class="mt-2">
+                <a href="#" @click.prevent="toggleLabelVisibility">
+                    {{ showAllLabels ? 'View less' : 'View more' }}
+                </a>
             </div>
         </div>
     </CheckboxGroup>
